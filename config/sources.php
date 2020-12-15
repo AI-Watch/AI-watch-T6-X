d753dff8922419c858cbd6cf7e23db9dfa9fa992
Lorenzino Vaccari <lorenzino.vaccari@gmail.com>
2020-12-15 19:40:35 +0100

    configured only with AI sources

--- config/sources.php
+++ config/sources.php
@@ -1,34 +1,34 @@
 <?php
 
-  $src["catalog"][1]  = "1-catalog-open-innovation-lombardia.csv";
-  $src["services"][1] = "1-services-open-innovation-lombardia.csv";
+//  $src["catalog"][1]  = "1-catalog-open-innovation-lombardia.csv";
+//  $src["services"][1] = "1-services-open-innovation-lombardia.csv";
 
-  $src["catalog"][2]  = "2-catalog-api-gouv-fr.csv";
-  $src["services"][2] = "2-services-api-gouv-fr.csv";
+//  $src["catalog"][2]  = "2-catalog-api-gouv-fr.csv";
+//  $src["services"][2] = "2-services-api-gouv-fr.csv";
 
-  $src["catalog"][3]  = "3-catalog-developers-italia.csv";
-  $src["services"][3] = "3-services-developers-italia.csv";
+// $src["catalog"][3]  = "3-catalog-developers-italia.csv";
+// $src["services"][3] = "3-services-developers-italia.csv";
 
-  $src["catalog"][4]  = "4-catalog-blockchain-for-digital-government-jrc115049.csv";
-  $src["services"][4] = "4-services-blockchain-for-digital-government-jrc115049.csv";
+//  $src["catalog"][4]  = "4-catalog-blockchain-for-digital-government-jrc115049.csv";
+//  $src["services"][4] = "4-services-blockchain-for-digital-government-jrc115049.csv";
 
-  $src["catalog"][5]  = "5-catalog-eublockchainforum-20200224.csv";
-  $src["services"][5] = "5-services-eublockchainforum-20200224.csv";
+//  $src["catalog"][5]  = "5-catalog-eublockchainforum-20200224.csv";
+//  $src["services"][5] = "5-services-eublockchainforum-20200224.csv";
 
-  $src["catalog"][6]  = "6-catalog-apis4dgov.csv";
-  $src["services"][6] = "6-services-apis4dgov.csv";
+//  $src["catalog"][6]  = "6-catalog-apis4dgov.csv";
+//  $src["services"][6] = "6-services-apis4dgov.csv";
 
-  $src["catalog"][7]  = "7-catalog-emerging-technologies-dg-grow.csv";
-  $src["services"][7] = "7-services-emerging-technologies-dg-grow.csv";
+//  $src["catalog"][7]  = "7-catalog-emerging-technologies-dg-grow.csv";
+//  $src["services"][7] = "7-services-emerging-technologies-dg-grow.csv";
   
-  $src["catalog"][8]  = "8-catalog-ai-robotics-vs-covid-19.csv";
-  $src["services"][8] = "8-services-ai-robotics-vs-covid-19.csv";
+//  $src["catalog"][8]  = "8-catalog-ai-robotics-vs-covid-19.csv";
+//  $src["services"][8] = "8-services-ai-robotics-vs-covid-19.csv";
   
   $src["catalog"][9]  = "9-catalog-ai-watch-t6-jrc120399.csv";
   $src["services"][9] = "9-services-ai-watch-t6-jrc120399.csv";
   
-  $src["catalog"][10]  = "10-catalog-digital-response-to-covid-19-joinup.csv";
-  $src["services"][10] = "10-services-digital-response-to-covid-19-joinup.csv";
+//  $src["catalog"][10]  = "10-catalog-digital-response-to-covid-19-joinup.csv";
+//  $src["services"][10] = "10-services-digital-response-to-covid-19-joinup.csv";
   
   $src["catalog"][11]  = "11-catalog-amsterdam-algorithm-register.csv";
   $src["services"][11] = "11-services-amsterdam-algorithm-register.csv";
@@ -39,7 +39,7 @@
   $src["catalog"][13]  = "13-catalog-chatbots-architecture-joinup.csv";
   $src["services"][13] = "13-services-chatbots-architecture-joinup.csv";
   
-  $src["catalog"][14]  = "14-catalog-covid-mobile-contact-tracing-apps.csv";
-  $src["services"][14] = "14-services-covid-mobile-contact-tracing-apps.csv";
+//  $src["catalog"][14]  = "14-catalog-covid-mobile-contact-tracing-apps.csv";
+//  $src["services"][14] = "14-services-covid-mobile-contact-tracing-apps.csv";
   
 ?>
