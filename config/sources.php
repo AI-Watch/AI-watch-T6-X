ca45a415512ed8af19736da4de0da32466bdc283
Lorenzino Vaccari <lorenzino.vaccari@gmail.com>
2020-12-15 19:45:11 +0100

    Changed sources with ONLY AIwatch sources

--- config/sources.php
+++ config/sources.php
@@ -30,14 +30,14 @@
 //  $src["catalog"][10]  = "10-catalog-digital-response-to-covid-19-joinup.csv";
 //  $src["services"][10] = "10-services-digital-response-to-covid-19-joinup.csv";
   
-  $src["catalog"][11]  = "11-catalog-amsterdam-algorithm-register.csv";
-  $src["services"][11] = "11-services-amsterdam-algorithm-register.csv";
+//  $src["catalog"][11]  = "11-catalog-amsterdam-algorithm-register.csv";
+//  $src["services"][11] = "11-services-amsterdam-algorithm-register.csv";
   
-  $src["catalog"][12]  = "12-catalog-helsinki-ai-register.csv";
-  $src["services"][12] = "12-services-helsinki-ai-register.csv";
+//  $src["catalog"][12]  = "12-catalog-helsinki-ai-register.csv";
+//  $src["services"][12] = "12-services-helsinki-ai-register.csv";
   
-  $src["catalog"][13]  = "13-catalog-chatbots-architecture-joinup.csv";
-  $src["services"][13] = "13-services-chatbots-architecture-joinup.csv";
+//  $src["catalog"][13]  = "13-catalog-chatbots-architecture-joinup.csv";
+//  $src["services"][13] = "13-services-chatbots-architecture-joinup.csv";
   
 //  $src["catalog"][14]  = "14-catalog-covid-mobile-contact-tracing-apps.csv";
 //  $src["services"][14] = "14-services-covid-mobile-contact-tracing-apps.csv";
