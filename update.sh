83173bd9f5b9977acf5e4009ecbb406bdc0f8650
Lorenzino Vaccari <lorenzino.vaccari@gmail.com>
2021-01-12 08:01:21 +0100

    delete-old-files

--- update.sh
+++ update.sh
@@ -2,7 +2,19 @@
 if [ $# -eq 0 ]; then
 	echo "No arguments supplied"
 else 
-	cd /Users/vaccalo/AIWatchT6/AI-watch-T6-X/scripts
+	cd /Users/vaccalo/AIWatchT6/AI-watch-T6-X
+	cd catalog
+	rm *.html
+	cd ../chart
+	rm *.html
+	cd ../data
+	rm *.js
+	rm *.json
+	cd ../norm
+	rm 9-*.json
+	cd ../service
+	rm *.html
+	cd ../scripts
 	./0-prepare-data.sh
 	./1-create-pages.sh
 	cd ..
